436ca82d6d342a7695592f2ee740e0ca89b16e89
kaja kruszewska <kruszewskakaja@wp.pl>
2021-09-07 18:10:40 +0200

    create chess_top module

---
+++ src/rtl/chess_top.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Kaja Kruszewska
+// 
+// Create Date:    18:07:14 07/09/2021
+// Design Name: 
+// Module Name:    chess_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module chess_top(
+      input wire clk, // ClkPort will be the board's 100MHz clk
+	  input wire rst
+
+    );
+	 
+
+	
+endmodule
