ffcfd542cb04f8dbe6fdda2f1e75bcb01f5ef5b2
kaja kruszewska <kruszewskakaja@wp.pl>
2021-09-08 13:16:52 +0200

    add debounce to chess and set special clock

--- src/rtl/chess_top.v
+++ src/rtl/chess_top.v
@@ -19,11 +19,44 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module chess_top(
-      input wire clk, // ClkPort will be the board's 100MHz clk
-	  input wire rst
-
+      	input wire clk, // ClkPort will be the board's 100MHz clk
+		input wire BtnL, input wire BtnU, input wire BtnD,input wire BtnR, BtnC,
+		input wire Reset // For reset   
     );
 	 
 
-	
+/* Clocking */
+//input clk;
+reg[26:0] DIV_CLK;
+wire full_clock;
+BUFGP CLK_BUF(full_clock, clk);
+
+always @(posedge full_clock, posedge Reset)
+begin
+	if (Reset) DIV_CLK <= 0;
+	else DIV_CLK <= DIV_CLK + 1'b1;
+end
+
+wire game_logic_clk, vga_clk, debounce_clk;
+assign debounce_clk = DIV_CLK[11]; // 24.4 kHz; needs to match game_logic for the single clock pulses
+
+/* Init debouncer */
+wire BtnC_pulse, BtnU_pulse, BtnR_pulse, BtnL_pulse, BtnD_pulse;
+debounce L_debounce(
+	.CLK(debounce_clk), .RESET(Reset),
+	.Btn(BtnL), .Btn_pulse(BtnL_pulse));
+debounce R_debounce(
+	.CLK(debounce_clk), .RESET(Reset),
+	.Btn(BtnR), .Btn_pulse(BtnR_pulse));
+debounce U_debounce(
+	.CLK(debounce_clk), .RESET(Reset),
+	.Btn(BtnU), .Btn_pulse(BtnU_pulse));
+debounce D_debounce(
+	.CLK(debounce_clk), .RESET(Reset),
+	.Btn(BtnD), .Btn_pulse(BtnD_pulse));
+debounce C_debounce(
+	.CLK(debounce_clk), .RESET(Reset),
+	.Btn(BtnC), .Btn_pulse(BtnC_pulse));
+
+
 endmodule
