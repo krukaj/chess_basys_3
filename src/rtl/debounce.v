15f8856ffedf67a3455433cace00547ec85a65b8
kaja kruszewska <kruszewskakaja@wp.pl>
2021-09-09 15:14:41 +0200

    resolve problem with debounce

--- src/rtl/debounce.v
+++ src/rtl/debounce.v
@@ -1,82 +1,74 @@
-// Listing 4.21
-module debounce (
-                input wire clk, reset,
-                input wire sw,
-                output reg db_level, db_tick
-        );
+// By Kaja Kruszewska
 
-        // symbolic state declaration
-        localparam  [1:0]
-                zero  = 2'b00,
-                wait0 = 2'b01,
-                one   = 2'b10,
-                wait1 = 2'b11;
 
-        // number of counter bits (2^N * 20ns = 40ms)
-        localparam N=21;
+module debounce(
+	input CLK,         // should be at about 24.4 kHz
+	input RESET,
+    input Btn,
+    output reg Btn_pulse);
 
-        // signal declaration
-        reg [N-1:0] q_reg, q_next;
-        reg [1:0] state_reg, state_next;
 
-        // body
-        // fsmd state & data registers
-        always @(posedge clk) begin
-                if (reset) begin
-                        state_reg <= zero;
-                        q_reg <= 0;
-                end else begin
-                        state_reg <= state_next;
-                        q_reg <= q_next;
-                end
-        end
-        // next-state logic & data path functional units/routing
-        always @* begin
-                state_next = state_reg;   // default state: the same
-                q_next = q_reg;           // default q: unchnaged
-                db_tick = 1'b0;           // default output: 0
+localparam INIT = 3'b000, WQ = 3'b001, SCEN_St = 3'b010, CCR = 3'b011, WFCR = 3'b100;
+reg[2:0] state;
 
-                case (state_reg)
-                zero: begin
-                        db_level = 1'b0;
-                        if (sw) begin
-                                state_next = wait1;
-                                q_next = {N{1'b1}}; // load 1..1
-                        end
-                end
-                wait1: begin
-                        db_level = 1'b0;
-                        if (sw) begin
-                                q_next = q_reg - 1;
-                                if (q_next==0) begin
-                                        state_next = one;
-                                        db_tick = 1'b1;
-                                end
-                        end else begin // sw==0
-                                state_next = zero;
-                        end
-                end
-                one: begin
-                        db_level = 1'b1;
-                        if (~sw) begin
-                                state_next = wait0;
-                                q_next = {N{1'b1}}; // load 1..1
-                        end
-                end
-                wait0: begin
-                        db_level = 1'b1;
-                        if (~sw) begin
-                                q_next = q_reg - 1;
-                                if (q_next==0)
-                                        state_next = zero;
-                        end else begin // sw==1
-                                state_next = one;
-                        end
-                end
+localparam max_i = 2000; // should yield a wait time of approx 0.25s
+reg[13:0] I;
 
-                default: state_next = zero;
+always @(posedge CLK, posedge RESET)
+begin
+	if (RESET) begin
+		Btn_pulse <= 0;
+		state <= INIT;
+		I <= 0;
+	end
+	else begin
+		case (state)
+			INIT:
+			begin
+				if (Btn) state <= WQ;
+				// else remain
 
-                endcase
-        end
+				I <= 0;
+			end
+
+			WQ:
+			begin
+				if (!Btn) state <= INIT;
+				else if (I == max_i) begin 
+					state <= SCEN_St;
+					Btn_pulse <= 1;
+				end
+				// else remain
+
+				I <= I + 1;
+			end
+
+			SCEN_St:
+			begin
+				state <= CCR;
+
+				Btn_pulse <= 0;
+				I <= 0;
+			end
+
+			CCR:
+			begin
+				if (!Btn) state <= WFCR;
+				// else remain
+
+				I <= 0;
+			end
+
+			WFCR:
+			begin
+				if (Btn) state <= CCR;
+				else if (I == max_i) state <= INIT;
+				// else remain
+
+				I <= I + 1;
+			end
+		endcase
+	end
+end
 
 endmodule
